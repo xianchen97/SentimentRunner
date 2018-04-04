7baf405d42744461d5cbcf1ed1d21ce65f06c011
Xian Chen <xianchen97@gmail.com>
2018-04-03 22:21:34 -0700

    Created base of the speech recognition api and the html template

---
+++ SpeechRecognition.js
@@ -0,0 +1,18 @@
+  /**
+  The following script requests access to the microphone and prints out the input onto the screen.
+  Documentation on the SpeechRecognition Web API can be found at
+  https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
+  **/
+
+  //Request speech regonition access, cross browser support.
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition || || window.mozSpeechRecognition || window.msSpeechRecognition;
+  //Create the speech recognition object.
+  const recognition = new SpeechRecognition();
+
+  //Property that defines whether results should be returned intermittently. -- Need to define whether it is random
+  recognition.interimResults = true;
+
+  //Set recognition language to english
+  recognition.lang = 'en-US';
+
+  
