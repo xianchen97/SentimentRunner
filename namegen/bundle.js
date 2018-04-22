1892d9fe937e15d61c1c091d0869863698ea2208
Xian Chen <xianchen97@gmail.com>
2018-04-22 11:17:34 -0700

    more updates

--- namegen/bundle.js
+++ namegen/bundle.js
@@ -3,15 +3,52 @@ var random_name = require('node-random-name');
 var male_or_f = 0;
 
 
-const body = document.getElementsByTagName("BODY")[0]
-        body.addEventListener('click', () => {
-          document.body.style.background = "#1E90FF";
-});
+onload = function(){
+  const fname = document.querySelector('#firstName');
+  const lname = document.querySelector('#lastName');
+  const body = document.querySelector('#body');
 
+  $("#babyButton").click(function(e) {
+     fname.textContent = firstName();
+     e.stopPropagation();
+  });
+
+  $("#babyButton").click(function(e) {
+     fname.textContent = firstName();
+     e.stopPropagation();
+  });
+
+  $('#lastName').bind('click',
+    function(e){
+        $(this).attr('contentEditable',true);
+        e.stopPropagation();
+
+    });
+
+  body.addEventListener('dblclick', () => {
+    if(male_or_f == 1){
+      male_or_f = 0;
+      document.body.style.background = "#FFCAD4"; 
+    }
+    else{
+      male_or_f = 1;
+      document.body.style.background = "#ADD8E6";
+    }
+
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    fname.textContent = possibleNames[0];
+  }, false);
+}
 window.firstName = function getRandomFullName(){
   var name =  random_name();
   var arr = name.split(" ");
-  return random_name({ first: true, gender: "male" })
+  if(male_or_f == 1){
+    return random_name({ first: true, gender: "male" });
+  }
+  else{
+    return random_name({ first: true, gender: "female" });
+  }
 }
 
 window.gender = function getNameGender(name){
--- namegen/namegen.js
+++ namegen/namegen.js
@@ -5,19 +5,49 @@ var male_or_f = 0;
 onload = function(){
   const fname = document.querySelector('#firstName');
   const lname = document.querySelector('#lastName');
-  const babyButton = document.querySelector('#babyButton');
-  babyButton.addEventListener('click', () => {
-  fname.textContent = firstName();
-});
+  const body = document.querySelector('#body');
 
-        document.addEventListener('DOMContentLoaded', function() {
-            fname.textContent = possibleNames[0];
-        }, false);
-      }
+  $("#babyButton").click(function(e) {
+     fname.textContent = firstName();
+     e.stopPropagation();
+  });
+
+  $("#babyButton").click(function(e) {
+     fname.textContent = firstName();
+     e.stopPropagation();
+  });
+
+  $('#lastName').bind('click',
+    function(e){
+        $(this).attr('contentEditable',true);
+        e.stopPropagation();
+
+    });
+
+  body.addEventListener('dblclick', () => {
+    if(male_or_f == 1){
+      male_or_f = 0;
+      document.body.style.background = "#FFCAD4"; 
+    }
+    else{
+      male_or_f = 1;
+      document.body.style.background = "#ADD8E6";
+    }
+
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    fname.textContent = possibleNames[0];
+  }, false);
+}
 window.firstName = function getRandomFullName(){
   var name =  random_name();
   var arr = name.split(" ");
-  return random_name({ first: true, gender: "male" })
+  if(male_or_f == 1){
+    return random_name({ first: true, gender: "male" });
+  }
+  else{
+    return random_name({ first: true, gender: "female" });
+  }
 }
 
 window.gender = function getNameGender(name){
